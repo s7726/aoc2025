61293e40f370fbd62c81c4168ba723798d366562
s7726 <gavinswanson@gmail.com>
2025-12-01 22:37:21 -0800

    WIP: Day 1 - Part 2

---
+++ day01/part2.py
@@ -0,0 +1,123 @@
+from os import DirEntry, curdir
+import unittest
+import re
+
+
+class TestPart(unittest.TestCase):
+    def test_example(self):
+        test_in = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
+
+        out = 6
+
+        start = 0
+        end = 99
+
+        start_num = 50
+
+        self.assertEqual(process(test_in, start, end, start_num), out)
+
+    def test_example2(self):
+        test_in = """R1000"""
+
+        out = 10
+
+        start = 0
+        end = 99
+
+        start_num = 50
+
+        self.assertEqual(process(test_in, start, end, start_num), out)
+
+    def test_example3(self):
+        test_in = """R393"""
+
+        out = 4
+
+        start = 0
+        end = 99
+
+        start_num = 43
+
+        self.assertEqual(process(test_in, start, end, start_num), out)
+
+    def test_example4(self):
+        test_in = """R460"""
+
+        out = 4
+
+        start = 0
+        end = 99
+
+        start_num = 40
+
+        self.assertEqual(process(test_in, start, end, start_num), out)
+
+
+def process(input, start, end, start_num):
+    total = 0
+    cur_index = start_num
+    for line in input.splitlines():
+        direction = line[:1]
+        increment = int(line[1:])
+        new_index = cur_index
+        if direction == "R":
+            new_index = cur_index + increment
+
+        if direction == "L":
+            new_index = cur_index - increment
+
+        adjusted_index = new_index
+
+        while adjusted_index > end:
+            if adjusted_index == end + 1:
+                adjusted_index = start
+            else:
+                adjusted_index = start + (adjusted_index - end - 1)
+            total += 1
+        while adjusted_index < start:
+            adjusted_index = end + 1 - (start - adjusted_index)
+            total += 1
+
+        print(
+            f"{cur_index} {direction} {increment} = {new_index} => {adjusted_index} ==> {total=}"
+        )
+        cur_index = adjusted_index
+    return total
+
+
+def input_runner(input):
+    """This exists to provide a cleaner place to feed puzzle specific arguments
+    to the process function
+    """
+
+    start = 0
+    end = 99
+
+    start_num = 50
+
+    return process(input, start, end, start_num)
+
+
+if __name__ == "__main__":
+    if unittest.main(exit=False).result.wasSuccessful():
+        file_content = None
+        with open("input.txt") as infile:
+            file_content = infile.read()
+
+        file_content = file_content[:-1]  # Strip trailing newline
+        import time
+
+        start_time = time.time()
+        output = input_runner(file_content)
+
+        print(f"=======  {(time.time() - start_time):.3f}s  ======")
+        print(f"Ans: {output}")
